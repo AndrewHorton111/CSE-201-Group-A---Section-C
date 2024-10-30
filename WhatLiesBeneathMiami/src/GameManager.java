c33dcb6776dec457b48cb552fef2aee2e2f6fb07
jacobartnak <jacobartnak@localhost>
2024-10-30 14:21:27 -0400

    jj

--- WhatLiesBeneathMiami/src/GameManager.java
+++ WhatLiesBeneathMiami/src/GameManager.java
@@ -1,9 +1,22 @@
+
 /**
- * 
+ * Class: CSE 201
+ * @author 
+ * @version 1.0
  */
 
 /**
+<<<<<<< HEAD
  * 
+=======
+<<<<<<< HEAD
+ * This is the master class that has all of the main method
+ * and everything needed to run the UI.
+=======
+ * testing123
+ * supp
+>>>>>>> branch 'master' of https://github.com/AndrewHorton111/CSE-201-Group-A---Section-C.git
+>>>>>>> branch 'master' of https://github.com/AndrewHorton111/CSE-201-Group-A---Section-C.git
  */
 public class GameManager {
 
