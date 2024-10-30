09bcd4d2108e7ddc37eae805ed8dd8ce3037deb1
jfollrath <follrajm@miamioh.edu>
2024-10-30 13:58:02 -0400

    This is my first time committing somehting in Eclipse.

---
+++ WhatLiesBeneathMiami/src/Character.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Character {
+
+	String name;
+	int health;
+	ArrayList<Object> inventory;
+	
+	public String getName() {
+		
+	}
+	
+	public int getHealth() {
+		
+	}
+	
+	public void setHealth(int newHealth) {
+		
+	}
+	
+	public void modifyHealth(int changeAmount) {
+		
+	}
+	
+	public ArrayList<Object> getInventory() {
+		
+	}
+	
+	
+}
