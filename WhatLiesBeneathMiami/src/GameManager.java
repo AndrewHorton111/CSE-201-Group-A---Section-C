78a909b9c92da0f057e70e3978abe141eb018a02
AndrewHorton111 <hortonal@miamioh.edu>
2024-11-01 11:27:15 -0400

    Minor changes to GameManager and Character classes.

--- WhatLiesBeneathMiami/src/GameManager.java
+++ WhatLiesBeneathMiami/src/GameManager.java
@@ -5,21 +5,15 @@
  * @version 1.0
  */
 
+//import java.util.ArrayList;
+
 /**
-<<<<<<< HEAD
- * 
-=======
-<<<<<<< HEAD
  * This is the master class that has all of the main method
  * and everything needed to run the UI.
-=======
- * testing123
- * supp
->>>>>>> branch 'master' of https://github.com/AndrewHorton111/CSE-201-Group-A---Section-C.git
->>>>>>> branch 'master' of https://github.com/AndrewHorton111/CSE-201-Group-A---Section-C.git
  */
 public class GameManager {
 
+	
 	/*
 	 * @param args
 	 */
@@ -28,13 +22,4 @@ public class GameManager {
 		// game
 
 	}
-	
-	
-	public void setHealth() {
-		System.out.print(false);
-	}
-	
-	// test boob
-	
-
 }
