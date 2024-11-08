f57475e81f0e3770baf1d6403bffaeb211f777b2
Adam Faglie <afaglie27@gmail.com>
2024-11-08 14:00:28 -0500

    Created outline for MiniBossRoom
    Merge branch 'master' of
    https://github.com/AndrewHorton111/CSE-201-Group-A---Section-C.git

--- WhatLiesBeneathMiami/src/MiniBossRoom.java
+++ WhatLiesBeneathMiami/src/MiniBossRoom.java
@@ -1,4 +1,6 @@
 /**
+ * @author Adam Faglie
+ * 
  * 
  */
 
@@ -10,8 +12,38 @@ import java.util.Map;
  */
 public class MiniBossRoom extends Room {
 
+	ArrayList<String> dialogList = new ArrayList<>;
+	dialogList.add("I'm better than you");
+	dialogList.add("You suck");
+	
+	ArrayList<Item> inventory = new ArrayList<>;
+	
+	Enemy miniBoss = new Enemy("Mini Boss", 50, this.inventory, this.dialogList);
+	
 	public MiniBossRoom(String roomDescription, Map<String, String> roomCommands, ArrayList<Item> items, ArrayList<String> objects) {
 		super(roomDescription, roomCommands, items, objects);
 	}
+	
+	/*
+	 * Starts fight with mini boss
+	 */
+	public void initiateCombat() {
+		
+	}
+	
+	/*
+	 * Calculates the characters damage taken during the fight
+	 */
+	public void calculateDamage() {
+		
+	}
+	
+	/*
+	 * displays mini boss's dialog during the fight
+	 */
+	public void displayDialog() {
+		
+	}
+	
 	
 }
