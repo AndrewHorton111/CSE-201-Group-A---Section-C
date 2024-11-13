e318ce8d9bcfc2b98651358bcf195c5b81214d41
jfollrath <follrajm@miamioh.edu>
2024-11-13 12:18:22 -0500

    RiddleRoom updates

--- WhatLiesBeneathMiami/src/RiddleRoom.java
+++ WhatLiesBeneathMiami/src/RiddleRoom.java
@@ -6,17 +6,25 @@
  */
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 
 public class RiddleRoom extends Room {
+	
+	public static void main(String[] args) {
+		printWelcome();
+		printRiddles();
+		
+	}
 	
 	public RiddleRoom(String roomDescription, Map<String, String> roomCommands, ArrayList<Item> items, ArrayList<String> objects) {
 		super(roomDescription, roomCommands, items, objects);
 	}
 
 
-	public void printWelcome() {
+	public static void printWelcome() {
 		System.out.println("Welcome to the riddle room");
 		System.out.println("To make it past you will need to answer my riddles correctly");
 		System.out.println("Are you prepared to test your intelligence?");
@@ -25,19 +33,28 @@ public class RiddleRoom extends Room {
 	}
 	
 	private void testMethod() {
-		System.out.println("TEST");
 		
-		
-		
+	}
+	
+	private static void printRiddles() {
+		// Load the riddles in
+		HashMap<Integer, String> riddles = riddleStorage();
+		// used for picking a random number that corresponds to a riddle
+		Random random = new Random();
+        Integer randomNumber = random.nextInt(3) + 1;
+        
+        // Print out the riddle here
+        System.out.println(riddles.get(randomNumber));
+        
 	}
 	
 	// To choose a riddle randomly put all of them in a map and assign a key that is 
 	// numbers 1-x. Then using random have that select the key at the start
-	private ArrayList<String> riddleStorage() {
-		ArrayList<String> riddles = new ArrayList<>();
-		riddles.add("The more you take the more you leave behind. What am I?");
-		riddles.add("I am easy to lift but hard to throw. What am I?");
-		riddles.add("What creature walks on four legs in the morning,"
+	private static HashMap<Integer, String> riddleStorage() {
+		HashMap<Integer, String> riddles = new HashMap<>();
+		riddles.put(1, "The more you take the more you leave behind. What am I?");
+		riddles.put(2, "I am easy to lift but hard to throw. What am I?");
+		riddles.put(3, "What creature walks on four legs in the morning,"
 				+ " two legs at noon, and three legs in the evening?");
 		
 		return riddles;
