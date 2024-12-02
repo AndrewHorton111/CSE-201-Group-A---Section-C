97ecd737996c5cc0dcc7716eabb159a3d4e2c40a
jfollrath <follrajm@miamioh.edu>
2024-12-02 12:21:33 -0500

    small change

--- WhatLiesBeneathMiami/src/GameManager.java
+++ WhatLiesBeneathMiami/src/GameManager.java
@@ -103,9 +103,6 @@ public class GameManager {
 				Room room2 = new FirstBattleRoom(roomDescription, roomCommands, items, objects);
 				roomList[i] = room2;
 				break;
-
-			// Riddle room setup. Not sure if this is in the proper room order.
-			// However this is an easy enough change to make
 			case 2:
 				roomDescription = "You move into a new mysterious room, there is something puzzling about your atmospher.\n"
 						+ " As you look around you notice a head on the wall and you approach it.\n"
